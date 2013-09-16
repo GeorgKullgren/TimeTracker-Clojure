4e1e3db258692b59d86b756e351651ee49ca29da
Georg Kullgren <georg@kullgren.org>
2013-09-16 15:59:58 +0200

    Added basic functionality.

--- src/timetracker_clojure/core.clj
+++ src/timetracker_clojure/core.clj
@@ -1,6 +1,23 @@
-(ns timetracker-clojure.core)
+(ns timetracker-clojure.core
+  (:require clj-time.local clj-time.core))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn getTime
+  []
+  (clj-time.local/local-now))
+
+(defn start-timer
+  []
+  (clj-time.local/local-now))
+
+(defn time-interval
+  [start-time stop-time]
+  (clj-time.core/interval start-time stop-time))
+  
+  
+
+(defn stop-timer
+  [start-time]
+  (time-interval start-time (clj-time.local/local-now)))
+  
+
+(stop-timer (start-timer))
